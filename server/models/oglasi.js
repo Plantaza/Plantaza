85fe58697d2adff5672f134ab7badaea7551ad2c
tilenmiklavic <tm7321@student.uni-lj.si>
2021-05-10 22:43:47 +0200

    Kategorije fix

--- server/models/oglasi.js
+++ server/models/oglasi.js
@@ -4,8 +4,8 @@ const { Uporabniki } = require('../controllers/uporabniki');
 
 const oglasiShema = new mongoose.Schema({
   _id: { type: String, required: true },
-  idRastline: { type: String, required: true },
-  idUporabnika: { type: String, required: true},
+  idRastline: { type: Rastline, required: true },
+  idUporabnika: { type: Uporabniki, required: true},
   slika: { type: String, required: false }
 })
 
