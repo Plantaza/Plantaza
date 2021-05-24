8bb5d662ad93f26d2156b22089921ec76fb59146
tilenmiklavic <tm7321@student.uni-lj.si>
2021-05-25 00:14:12 +0200

    Shranjevanje rastlin

--- app/src/app/components/favourites/favourites.component.ts
+++ app/src/app/components/favourites/favourites.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Oglas } from 'src/app/classes/oglas';
+import { OglasiService } from 'src/app/services/oglasi.service';
 
 @Component({
   selector: 'app-favourites',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavouritesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private oglasiStoritev: OglasiService
+  ) { }
+
+  oglasi: Oglas[]
 
   ngOnInit(): void {
+    this.oglasiStoritev.pridobiOglase()
+      .then(odgovor => {
+        this.oglasi = odgovor
+      })
   }
-
 }
